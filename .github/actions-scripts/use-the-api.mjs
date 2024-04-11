49c98e430227401612934a5b453c1e7509faf0c4
thejaswitricon <thejaswi.cy@triconinfotech.com>
2024-04-11 14:25:28 +0530

    ci: test

--- .github/actions-scripts/use-the-api.mjs
+++ .github/actions-scripts/use-the-api.mjs
@@ -26,6 +26,6 @@ async function addCollaborators(owner, repo, teamSlugs) {
 // Usage example
 const owner = 'tandfgroup'; // Replace 'OWNER' with the repository owner's username or organization name
 const repo = 'codespaces-demo'; // Replace 'REPO' with the name of the repository
-const teamSlugs = ['tandfgroup/platform-engineering', 'tandfgroup/quality-engineering']; // Array of team slugs
+const teamSlugs = ['platform-engineering', 'quality-engineering']; // Array of team slugs
 
 addCollaborators(owner, repo, teamSlugs);
